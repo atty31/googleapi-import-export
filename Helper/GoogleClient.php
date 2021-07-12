7828e28f70ce1bc24a80f69ae7e85f362d0946b1
Attila <naghi.attila@gmail.com>
2021-07-12 20:12:39 +0300

    Code clean up.

--- Helper/GoogleClient.php
+++ Helper/GoogleClient.php
@@ -4,11 +4,14 @@
 namespace Atma\Products\Helper;
 
 use Atma\Products\Helper\General as GeneralHelper;
-use Google_Service_Analytics;
-use Google_Service_Sheets;
+use Google\Model;
+use Google\Service\Sheets;
 use Google_Client;
 use Google_Service_Sheets_Spreadsheet;
 use Exception;
+use Google_Service_Sheets_ValueRange;
+use Google\Exception as GoogleException;
+use Psr\Log\LoggerInterface;
 
 class GoogleClient
 {
@@ -17,14 +20,31 @@ class GoogleClient
      */
     public $generalHelper;
 
+    /**
+     * @var LoggerInterface
+     */
+    public $logger;
+
+    /**
+     * GoogleClient constructor.
+     * @param General $generalHelper
+     * @param LoggerInterface $logger
+     */
     public function __construct(
-        GeneralHelper $generalHelper
+        GeneralHelper $generalHelper,
+        LoggerInterface $logger,
     ){
         $this->generalHelper = $generalHelper;
+        $this->logger = $logger;
     }
 
+    /**
+     * @return Google_Client
+     * @throws GoogleException
+     */
     public function getGoogleClient()
     {
+        $spreadsheetId = $this->generalHelper->getSpreadSheetId();
         $client = new Google_Client();
 
         $client->setApplicationName('Google Sheets and PHP');
@@ -33,13 +53,44 @@ class GoogleClient
         $client->setAuthConfig($credentials);
         $client->setScopes([Google_Service_Sheets::SPREADSHEETS]);
         $client->setAccessType('offline');
+
+        return $client;
+
+    }
+    public function readSpreadSheet(Google_Client $client)
+    {
         $service = new Google_Service_Sheets($client);
         try {
-            $response = $service->spreadsheets_values->get($this->generalHelper->getSpreadSheetId(), 'Import!A:Z');
+            $response = $service->spreadsheets_values->get($spreadsheetId, 'Import!A:Z');
         }catch (Exception $error){
-            var_dump($error->getMessage()); exit();
+            $this->logger->error($error->getMessage());
         }
-        var_dump($response->getValues());
+        return $response->getValues();
+    }
 
+    /**
+     * @param Google_Client $client
+     * @return mixed
+     */
+    public function updateSpreadSheet(Google_Client $client)
+    {
+        $values = [
+            ['col 1', 'col 2', 'col 3'],
+            ['col 1', 'col 2']
+        ];
+
+        $body = new Google_Service_Sheets_ValueRange([
+            "values" => $values
+        ]);
+
+        $params = [
+            'valueInputOption' => 'RAW'
+        ];
+        try {
+            $updateSheet = $service->spreadsheets_values->update($spreadsheetId, 'Export!A2:Z', $body, $params);
+        }catch (Exception $error){
+            $this->logger->error($error->getMessage());
+        }
+        return $updateSheet;
     }
 }
